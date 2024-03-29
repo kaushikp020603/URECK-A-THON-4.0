61039872f05ca9dabfa317fc9bb4806938938280
kaushik020603 <kaushik020603@gmail.com>
2024-03-29 23:36:33 +0530

    added dashboard

---
+++ dashboard.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import subprocess
+
+# Title and description
+st.title('Study Buddy')
+st.write("Welcome to the Study Buddy  Dashboard. Explore various functionalities with just a click!")
+
+# Button options with icons
+options = {
+    "🗓️ Task Manager": "task_manager.py",
+    "👩‍🎓 Schedule Manager": "schedule_manager.py",
+    "📋 Quiz Generator": "gui.py",
+    "📚 Library": "library.py"
+}
+
+# Display buttons vertically
+st.subheader("Select an Option")
+for option, file in options.items():
+    if st.button(option):
+        subprocess.Popen(["streamlit", "run", file], shell=True)
